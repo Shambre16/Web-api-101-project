b4bd16be2f2ce9e12531647f88fa17a044789da8
Shambre <ssenawright@techtonicgroup.com>
2018-05-09 16:01:00 -0600

    Commits and pushes all recent changes.  This push has local storage saving the most recent search and has added maps to each meetUp location card.

--- js/script.js
+++ js/script.js
@@ -1,59 +1,90 @@
 class DataRequest {
-  constructor(oArgs) {
+  constructor(instanceKey, key) {
     // this.$container = $("div.enclose");
-    this.dataUrl = "https://api.meetup.com/2/cities?key=482a804c7b4414151e743a1a7b1f5156&";
+    this.dataUrl = "https://api.meetup.com/2/cities?";
+    this.keyInstance = instanceKey;
+    this.key = key;
   }
 
   init() {
     this.$searchBtn = $("#searchBtn");
-    this.$responseField = ("#displayResults");
-    this.$cloneCard = $(".clone-card div").clone();
 
-    this.bindEvents();
-    return false;
+    this.map = new Map();
+    this.map.initMap();
+
+
+  this.bindEvents();
+  return false;
+}
+
+
+bindEvents() {
+  $("#searchBtn").on("click", $.proxy(this.refreshData, this));
+
+}
+
+refreshData(event) {
+  event.preventDefault();
+  let values = $("form").serialize();
+  $.ajax({
+      dataType: 'jsonp',
+      type: "GET",
+      url: this.dataUrl + values,
+      data: {
+        key: this.key
+      }
+    }).done(this.successfulQuery)
+    .fail(function() {
+      console.log("fail")
+    })
+};
+
+
+successfulQuery(response) {
+  localStorage.setItem("meetupResults", JSON.stringify(response.results));
+  var singleResponseArray = "";
+  $("ul").remove();
+  for (var i = 0; i < response.results.length; i++) {
+    singleResponseArray +=
+      `<ul class='list-group'><img class="card-img-top" id="map" src="https://maps.googleapis.com/maps/api/staticmap?center=
+      ${response.results[i].lat},${response.results[i].lon}&zoom=10&size=300x300&key=AIzaSyCWHWgvvHMmE4ZAF1-HipxMefI6C8-c0yI" alt="Card image cap">
+      <li class='list-group-item zip'>Zip Code:<span></span> ${response.results[i].zip} </li>
+      <li class='list-group-item country'>Country:<span></span> ${response.results[i].localized_country_name} </li>
+      <li class='list-group-item city'>City:<span></span> ${response.results[i].city} </li>
+      <li class='list-group-item ranking'>Ranking:<span></span> ${response.results[i].ranking} </li>
+      <li class='list-group-item state'>State:<span></span> ${response.results[i].state} </li>
+      <li class='list-group-item member_count'>Member Count:<span></span> ${response.results[i].member_count} </li></ul>`;
   }
 
-  bindEvents() {
-    $("#searchBtn").on("click", $.proxy(this.refreshData, this));
+  $("#displayResults").append(singleResponseArray);
 
-  }
+}
 
-  refreshData(event) {
-      event.preventDefault();
-      let values = $( "form" ).serialize();
-      $.ajax({
-          dataType: 'jsonp',
-          type:"GET",
-          url: this.dataUrl + values,
-          data: {
-          }
-        }).done(this.successfulQuery)
-        .fail(function(){
-          console.log("fail")
-        })
-    };
 
-  successfulQuery(response){
-    var singleResponseArray= "";
-    $("ul").remove();
-    for (var i = 0; i < response.results.length; i++) {
-      singleResponseArray +=
-      "<ul class='list-group'><li class='list-group-item zip'>Zip Code:<span></span> " + response.results[i].zip + "</li>" +
-      "<li class='list-group-item country'>Country:<span></span> " + response.results[i].localized_country_name + "</li>" +
-      "<li class='list-group-item city'>City:<span></span> " + response.results[i].city + "</li>" +
-      "<li class='list-group-item ranking'>Ranking:<span></span> " + response.results[i].ranking + "</li>" +
-      "<li class='list-group-item state'>State:<span></span> " + response.results[i].state + "</li>" +
-      "<li class='list-group-item member_count'>Member Count:<span></span> " + response.results[i].member_count + "</li></ul>";
+
+
+getObject(instanceKey) {
+  let response = JSON.parse(localStorage.getItem("meetupResults"));
+  if (response) {
+    for (let i = 0; i < response.length; i++) {
+      let result = response[i];
+      console.log(result);
     }
-
-    $("#displayResults").append(singleResponseArray);
-
+    return true;
+  }
 }
 
 }
 
-$(document).ready(function(){
-DataRequestModule.init();
+class Map {
+  constructor(){
+    return this;
+  }
+  initMap() {
+  }
+}
+
+$(document).ready(function() {
+  var DataRequestModule = new DataRequest("meetupResults","482a804c7b4414151e743a1a7b1f5156");
+  DataRequestModule.init();
 })
-
-var DataRequestModule = new DataRequest();
